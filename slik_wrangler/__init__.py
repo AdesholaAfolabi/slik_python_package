4f85146e0b487ff5110d4d53beffe9b43361908b
Akinwande <akinwandekomolafe@gmail.com>
2022-04-05 07:21:17 +0100

    renamed package

---
+++ slik_wrangler/__init__.py
@@ -0,0 +1,5 @@
+from slik_wrangler import loadfile
+from slik_wrangler import preprocessing
+from slik_wrangler import pipeline
+from slik_wrangler import utils
+from slik_wrangler import plot_funcs
