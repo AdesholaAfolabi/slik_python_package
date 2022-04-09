34770647b63da5e695f6303de16526e222bb289f
ganiyuolalekan <ganiyuolalekanmatthew@gmail.com>
2022-04-09 01:45:29 +0100

    Update on Change Case

--- slik_ml/preprocessing.py
+++ slik_ml/preprocessing.py
@@ -56,7 +56,7 @@ def bin_age(dataframe=None, age_col=None, add_prefix=True):
     return data
 
 
-def change_case(dataframe=None ,column=None,case='lower'):
+def change_case(dataframe, column=None, case='lower', inplace=False):
 
     """
     Change the case of a pandas series to either upper or lower case
@@ -65,43 +65,57 @@ def change_case(dataframe=None ,column=None,case='lower'):
     ----------
     dataframe: Dataframe or named Series
     
-    column: str.
-        The column to perform the operation on
+    column: str, list
+        The column or list of columns to perform the operation on
     
     case: str. Default is set to lower 
         Indicates the type of operation to perform
+    
+    inplace: bool. Default is set to False
+        Indicates if changes should by made within the dataframe or not.
 
     Returns
     -------
     Pandas Dataframe:
     """
         
-    if dataframe is None:
-        raise ValueError("data: Expecting a DataFrame or Series, got 'None'")
+    if type(dataframe) != pd.DataFrame and type(dataframe) != pd.Series:
+        raise ValueError(
+            "data: Expecting a DataFrame or Series, got 'None'"
+        )
         
-    if not isinstance(column, str):
-        errstr = f'The given type for column is {type(column).__name__}. Expected type is a string'
-        raise TypeError(errstr)
-
-    df = dataframe.copy()
+    if isinstance(column, str) or isinstance(column, str):
+        raise TypeError(
+            f'The given type for column is {type(column).__name__}. Expected type is a string or list'
+        )
+    
+    if not inplace:
+        dataframe = dataframe.copy()
 
     def capitalize_case(x):
-        list_of_str = x.split(' ')
-        new_list =[]
-        for item in list_of_str:
-            new_list.append(item.capitalize())
-        JoinedStr = ' '.join(new_list)
-        return JoinedStr
+        """
+        Capitalize Case Function
+        """
+        
+        return ' '.join([
+            item.capitalize() 
+            for item in x.split(' ')
+        ])
     
-    if case == 'lower':
-        df[column] = df[column].apply(lambda x: x.lower())
-        return df
-    elif case == 'upper':
-        df[column] = df[column].apply(lambda x: x.upper())
-        return df
-    elif case=='capitalize':
-        df[column] = df[column].apply(lambda x: capitalize_case(x))
-        return df
+    def perform_case(func):
+        dataframe[column] = dataframe[column].applymap(func)
+        
+    action = {
+        'lower': lambda x: x.lower(),
+        'upper': lambda x: x.upper(),
+        'capitalize': lambda x: capitalize_case(x)
+    }
+    
+    if case in action.keys():
+        perform_case(action[case])
+        
+        if not inplace:
+            return dataframe
     else:
         raise ValueError(f"case: expected one of upper,lower or captitalize got {case}")
     
